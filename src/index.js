4d673ae202d02feaf6155a020b171766489b0e00
ishowshao <ishowshao@me.com>
2020-08-28 18:10:24 +0800

    dev

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Clock from './Clock';
 import * as serviceWorker from './serviceWorker';
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+function Welcome(props) {
+  return (
+    <div>
+      <h1>Hello, {props.name}</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function Item(props) {
+  return <li>{props.content}</li>;
+}
+
+function List(props) {
+  return <ul><Item></Item></ul>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Clock />,
   document.getElementById('root')
 );
 
